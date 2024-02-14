caf73d6ece01db16f69a99297aa91c321dba2c35
knieth <buancneth@gmail.com>
2024-02-14 11:31:41 +0800

    demo setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './index.html',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Montserrat', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
